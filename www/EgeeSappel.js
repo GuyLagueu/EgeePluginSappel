875469bff282d99a14c993984ffd3342c0382ecc
lagueu <lagson2@yahoo.fr>
2018-12-15 23:58:12 +0100

    Initial commit

---
+++ www/EgeeSappel.js
@@ -0,0 +1,12 @@
+var exec = require('cordova/exec');
+
+
+module.exports.add = function (arg0, success, error)
+{
+    exec(success, error, 'EgeeSappel', 'add', [arg0])
+};
+
+module.exports.substract = function (arg0, success, error) 
+{
+    exec(success, error, 'EgeeSappel', 'substract', [arg0])
+};
